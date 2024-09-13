ed2399f24dd7e1ec764fcd0d2566afea5fe4a503
TigerHu <yuanxuehu2016@126.com>
2024-09-13 08:00:49 +0800

    Initial Commit

---
+++ KJBluetooth/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  KJBluetooth
+//
+//  Created by TigerHu on 2024/9/13.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ KJBluetooth/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  KJBluetooth
+//
+//  Created by TigerHu on 2024/9/13.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
